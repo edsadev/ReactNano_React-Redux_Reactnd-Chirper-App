b8154ca498b5aea79cc672c227fd2e23708e0df7
Edmundo0994 <esala.094@gmail.com>
2020-05-30 21:30:17 -0500

    feat: Now you can like and unlike a tweet

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,8 +1,34 @@
+import { saveLikeToggle } from '../utils/api'
+
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
+export const TOGGLE_TWEET = 'TOGGLE_TWEET'
 
 export function receiveTweets (tweets) {
   return {
     type: RECEIVE_TWEETS,
     tweets
+  }
+}
+
+function toggleTweet({ id, authedUser, hasliked}) {
+  return {
+    type:TOGGLE_TWEET,
+    id,
+    authedUser,
+    hasliked
+  }
+}
+
+export function handleToggleTweet (info) {
+  return (dispatch) => {
+    // Optimistic Updating
+    dispatch(toggleTweet(info))
+
+    return saveLikeToggle(info)
+      .catch((e) => {
+        console.warn('Error in handleToggleTweet: ', e)
+        dispatch(toggleTweet(info))
+        alert('There was an error liking the tweet. Try again!')
+      })
   }
 }
